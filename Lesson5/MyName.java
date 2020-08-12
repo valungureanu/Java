8785b6a91ec9339710479c0ce67ca189401f75d4
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-12 21:33:17 +0300

    Al 3-lea program!!!

--- Lesson5/MyName.java
+++ Lesson5/MyName.java
@@ -4,7 +4,7 @@ public class MyName
 	public static void main(String[] args)
 	{
 		
-		System.out.println("My name is Valeriu Ungureanu!");
+		System.out.println("My name is Valeriu Ungureanu, a person who begins programming in Java Language!");
 		
 	}
 }
