348fe99fda8ff0b0a741f7ac6fdf127e44023d30
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-15 09:41:48 +0300

    Proba a Inteliij Idea

---
+++ src/Lesson6.java
@@ -0,0 +1,26 @@
+public class Lesson6 {
+    public static void main(String[] parms) {
+        Lesson6 primaLectie = new Lesson6();
+        Test1 primaClasa = new Test1();
+
+        byte b = 60;
+        short s = 50;
+        int i = 400000;
+        long l = 1000000L;
+        float f = 12.5F;
+        double d = 13;
+        char c = 'A";
+        String s1 = "";
+        boolean bool = true;
+
+        int a1 = 10;
+        int b1 = 20;
+        int sum = a1 + b1;
+
+        String concatString ="String1"+"String2";
+
+        System.out.println(d+ "  " + f);
+
+
+    }
+}
