d52e5cd9b9f7c9c5d8067e7039e7c3bc9bbff2c7
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-18 12:58:09 +0300

    Un commit dupa ce am facut testari cu tipuri de date!

--- Lesson6/src/test.java
+++ Lesson6/src/test.java
@@ -2,11 +2,7 @@ public class test {
     int numarDeUsi = 3;
     double capacitateMotor = 1.4;
     boolean masajScaunSofer = false;
-    String denumireaCompleta = "Mercedes Benz A-Class A140"
+    String denumireaCompleta = "Mercedes Benz A-Class A140";
 
-    public void accelereaza() {
-    }
-
-    public void accelereaza() {
-    }
 }
+
