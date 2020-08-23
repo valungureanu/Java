98d261e60a062c8fb011325bf97e975382247f3e
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-23 14:24:40 +0300

    Crearea claselor si utilizarea lor

--- Lesson6/src/Angajat.java
+++ Lesson6/src/Angajat.java
@@ -1,2 +1,28 @@
 public class Angajat {
+     String nume="Ion";
+     long virsta=56;
+
+    Angajat() {
+
+    }
+
+    long damiVirsta() {
+        return virsta;
+    }
+
+    void printeazaNume() {
+        System.out.println(virsta);
+    }
+
+    void seteazaVirsta(long argumentVirsta) {
+        virsta = argumentVirsta;
+    }
+
+    void printeazaVirstaSiNume() {
+        System.out.println("Virsta: " +virsta + " " + "Nume " + nume);
+    }
+
+
+
+
 }
