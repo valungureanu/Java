755fc7cd86b3a5d23d216c468ca23f5be1bb95ee
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-23 14:42:12 +0300

    Stergerea directoriului Lesson6

--- Lesson6/src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    int numarDeUsi = 3;
-    double capacitateMotor = 1.4;
-    boolean masajScaunSofer = false;
-    String denumireaCompleta = "Mercedes Benz A-Class A140";
-
-}
-
