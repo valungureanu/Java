827af923f8a2e89c3bd238c09f761c5f344166bb
Valeriu Ungureanu <v.a.ungureanu@gmail.com>
2020-08-27 18:45:18 +0300

    Experimente in timpul lectiei nr. 12!

--- Lessons/src/md/tekwill/pachet3/Clasa3.java
+++ Lessons/src/md/tekwill/pachet3/Clasa3.java
@@ -7,7 +7,7 @@ public class Clasa3 {
     public static void main(String[] args) {
         Clasa1 ob1 = new Clasa1();
         ob1.met11();
-        // ob1.met21();//java: met21() has protected access in md.tekwill.pachet1.Clasa1
+        //ob1.met21();//java: met21() has protected access in md.tekwill.pachet1.Clasa1
         //ob1.met31();//java: met31() is not public in md.tekwill.pachet1.Clasa1; cannot be accessed from outside package
         //ob1.met41();//java: met41() has private access in md.tekwill.pachet1.Clasa1
 
--- Lessons/src/md/tekwill/pachet3/Clasa4.java
+++ Lessons/src/md/tekwill/pachet3/Clasa4.java
@@ -3,20 +3,19 @@ package md.tekwill.pachet3;
 public class Clasa4 {
 
     public static void met14() {
-        System.out.println("Metoda 1 din Pachetul 4");
-        met24();
-        met34();
+        System.out.println("Metoda 1 din Clasa 4");
+
     }
 
     private static void met24() {
-        System.out.println("Metoda 2 din Pachetul 4");
+        System.out.println("Metoda 2 din Clasa 4");
     }
 
     private static void met34() {
-        System.out.println("Metoda 3 din Pachetul 4");
+        System.out.println("Metoda 3 din Clasa 4");
     }
 
     public static void main(String[] args) {
-        met14();
+        met34();
     }
 }
